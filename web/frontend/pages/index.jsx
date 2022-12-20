e180c565a3c65701713aba8d6f865ed455578828
lucvil <yoshimura037@g.ecc.u-tokyo.ac.jp>
2022-12-20 18:14:52 +0900

    make chart prototype

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -4,16 +4,39 @@ import {
   EmptyState,
   Layout,
   Page,
-  SkeletonBodyText,
   SettingToggle,
-  Button
 } from "@shopify/polaris";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
 
 import {useState, useCallback} from 'react';
 
 import { useAppQuery } from "../hooks";
 
 let isFirst = true;
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
 
 export default function HomePage() {
   /*
@@ -75,7 +98,7 @@ export default function HomePage() {
     });
   }
   
-
+  //scriptTagの切り替えボタン
   function SettingScriptTagToggle() {
 
     const handleToggle = useCallback(() => {
@@ -101,20 +124,58 @@ export default function HomePage() {
     }
   }
 
+  // チェック件数の折れ線グラフ
+  function CheckCountChart(){
+    const options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Line Chart',
+        },
+      },
+    };
+
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: labels.map(() => 550),
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+      ],
+    };
+
+    return (
+      <Line options={options} data={data} />
+    );
+  }
+
+
   return (
     <Page>
-      {/* <TitleBar
+      <Layout>
+        <Layout.Section>
+          {SettingScriptTagToggle()}
+          {CheckCountChart()}
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+
+
+        {/* <TitleBar
         title="test_botton"
         primaryAction={{
           content: "test_botton",
           onAction: () => pass,
         }}
       /> */}
-      <Layout>
-        <Layout.Section>
-          {SettingScriptTagToggle()}
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
 }
